5bea3e3f7f95152e00932f78914e31028a55c32e
Shreya <shreyayogeshkhadilkar@gmail.com>
2025-07-31 16:00:55 +0530

    Moved Server folder to project root

--- Server/server.js
+++ Server/server.js
@@ -20,3 +20,5 @@ app.get('/', (req,res)=> res.send("API is Working"))
 
 const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// Server folder moved to root
